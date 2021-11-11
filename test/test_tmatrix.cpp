0d74fdf2f0b8d0f9c07e3d475599ea57b198915d
Korobeynikova Alice <iuuyyttttt@mail.ru>
2021-11-11 12:16:28 +0300

    Tests refactoring

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -2,6 +2,38 @@
 
 #include <gtest.h>
 
+namespace
+{
+	template <class Type>
+	Type ElementsNumberFunction(int i, int j, int theSize)
+	{
+		return i * theSize + j;
+	}
+
+	template <class Type>
+	Type ConstantFunction(int i, int j, Type theConstant)
+	{
+		return theConstant;
+	}
+
+	// Returns a vector TVector<Type>, each element of which is initialized with a functor theFunc
+	// that takes one required argument - the indices of the currently initialized element.
+	// Also, this functor can have an arbitrary number of other arguments - theArgs.
+	template <class Type, class Functor, class... ArgsType>
+	TMatrix<Type> CreateMatrix(const int theSize, Functor theFunc, ArgsType... theArgs)
+	{
+		TMatrix<Type> aMatrix(theSize);
+		for (int i = 0; i < theSize; ++i)
+		{
+			for (int j = i; j < theSize; ++j)
+			{
+				aMatrix[i][j] = theFunc(i, j, theArgs...);
+			}
+		}
+		return aMatrix;
+	}
+}
+
 TEST(TMatrix, can_create_matrix_with_positive_length)
 {
   ASSERT_NO_THROW(TMatrix<int> m(5));
@@ -27,14 +59,7 @@ TEST(TMatrix, can_create_copied_matrix)
 TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 {
 	const int size = 10;
-	TMatrix<int> m1(size);
-	for (int i = 0; i < size; ++i)
-	{
-		for (int j = i; j < size; ++j)
-		{
-			m1[i][j] = i * size + j;
-		}
-	}
+	TMatrix<int> m1 = CreateMatrix<int>(size, ElementsNumberFunction<int>, size);
 	TMatrix<int> m2(m1);
 	ASSERT_EQ(m1, m2);
 }
@@ -42,14 +67,7 @@ TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 TEST(TMatrix, copied_matrix_has_its_own_memory)
 {
 	const int size = 10;
-	TMatrix<int> m1(size);
-	for (int i = 0; i < size; ++i)
-	{
-		for (int j = i; j < size; ++j)
-		{
-			m1[i][j] = i * size + j;
-		}
-	}
+	TMatrix<int> m1 = CreateMatrix<int>(size, ElementsNumberFunction<int>, size);
 	TMatrix<int> m2(m1);
 	ASSERT_NE(&(m1[0][0]), &(m2[0][0]));
 }
@@ -81,15 +99,7 @@ TEST(TMatrix, throws_when_set_element_with_too_large_index)
 
 TEST(TMatrix, can_assign_matrix_to_itself)
 {
-	TMatrix<int> v(10);
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int j = i; j < 10; ++j)
-		{
-			v[i][j] = i * 10 + j;
-		}
-		
-	}
+	TMatrix<int> v = CreateMatrix<int>(10, ElementsNumberFunction<int>, 10);
 	TMatrix<int> v1(v);
 	v = v;
 	ASSERT_EQ(v, v1);
@@ -97,16 +107,9 @@ TEST(TMatrix, can_assign_matrix_to_itself)
 
 TEST(TMatrix, can_assign_matrices_of_equal_size)
 {
-	TMatrix<int> v(10);
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int j = i; j < 10; ++j)
-		{
-			v[i][j] = i * 10 + j;
-		}
-
-	}
-	TMatrix<int> v1(10);
+	int size = 10;
+	TMatrix<int> v = CreateMatrix<int>(size, ElementsNumberFunction<int>, size);
+	TMatrix<int> v1(size);
 	v1 = v;
 	ASSERT_EQ(v, v1);
 }
@@ -121,60 +124,49 @@ TEST(TMatrix, assign_operator_change_matrix_size)
 
 TEST(TMatrix, can_assign_matrices_of_different_size)
 {
-	TMatrix<int> v(10);
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int j = i; j < 10; ++j)
-		{
-			v[i][j] = i * 10 + j;
-		}
-
-	}
-	TMatrix<int> v1(5);
-	ASSERT_NO_THROW(v1 = v);
+	TMatrix<int> v = CreateMatrix<int>(10, ElementsNumberFunction<int>, 10);
+	TMatrix<int> v1 = CreateMatrix<int>(5, ConstantFunction<int>, 5);
+	v1 = v;
+	ASSERT_EQ(v1, v);
 }
 
 TEST(TMatrix, compare_equal_matrices_return_true)
 {
-	TMatrix<int> v(10);
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int j = i; j < 10; ++j)
-		{
-			v[i][j] = i * 10 + j;
-		}
-
-	}
-	TMatrix<int> v1(v);
+	int size = 10;
+	TMatrix<int> v = CreateMatrix<int>(size, ElementsNumberFunction<int>, size);
+	TMatrix<int> v1 = CreateMatrix<int>(size, ElementsNumberFunction<int>, size);
 	ASSERT_EQ(v, v1);
 }
 
 TEST(TMatrix, compare_matrix_with_itself_return_true)
 {
-	TMatrix<int> v(10);
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int j = i; j < 10; ++j)
-		{
-			v[i][j] = i * 10 + j;
-		}
-
-	}
+	TMatrix<int> v = CreateMatrix<int>(10, ElementsNumberFunction<int>, 10);
 	ASSERT_EQ(v, v);
 }
 
 TEST(TMatrix, matrices_with_different_size_are_not_equal)
 {
-	TMatrix<int> v(10);
-	TMatrix<int> v1(5);
+	TMatrix<int> v = CreateMatrix<int>(10, ConstantFunction<int>, 0);
+	TMatrix<int> v1 = CreateMatrix<int>(5, ConstantFunction<int>, 0);
 	ASSERT_NE(v, v1);
 }
 
 TEST(TMatrix, can_add_matrices_with_equal_size)
 {
-	TMatrix<int> v(10);
-	TMatrix<int> v1(10);
-	ASSERT_NO_THROW(v + v1);
+	int size = 10;
+	int constant = 5;
+	TMatrix<int> v = CreateMatrix<int>(size, ElementsNumberFunction<int>, size);
+	TMatrix<int> v1 = CreateMatrix<int>(size, ConstantFunction<int>, constant);
+
+	TMatrix<int> expected = CreateMatrix<int>(10,
+		[](int i, int j, int size, int value)
+		{
+			return ElementsNumberFunction<int>(i, j, size) + ConstantFunction(i, j, value);
+		},
+		size, constant);
+
+	TMatrix<int> actual = v + v1;
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TMatrix, cant_add_matrices_with_not_equal_size)
@@ -188,16 +180,27 @@ TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 
  / \\\\\\\\\
 /   \\\\\\\\\
-=====        |
+=====        |    - это домик
 |   |        |
 =====     -
 
 */
 TEST(TMatrix, can_subtract_matrices_with_equal_size)
 {
-	TMatrix<int> v(10);
-	TMatrix<int> v1(10);
-	ASSERT_NO_THROW(v - v1);
+	int size = 10;
+	int constant = 5;
+	TMatrix<int> v = CreateMatrix<int>(size, ElementsNumberFunction<int>, size);
+	TMatrix<int> v1 = CreateMatrix<int>(size, ConstantFunction<int>, constant);
+
+	TMatrix<int> expected = CreateMatrix<int>(10,
+		[](int i, int j, int size, int value)
+		{
+			return ElementsNumberFunction<int>(i, j, size) - ConstantFunction(i, j, value);
+		},
+		size, constant);
+
+	TMatrix<int> actual = v - v1;
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -2,6 +2,35 @@
 
 #include <gtest.h>
 
+namespace
+{
+	template <class Type>
+	Type IdentityFunction(int i)
+	{
+		return i;
+	}
+
+	template <class Type>
+	Type ConstantFunction(int i, Type theConstant)
+	{
+		return theConstant;
+	}
+
+	// Returns a vector TVector<Type>, each element of which is initialized with a functor theFunc
+	// that takes one required argument - the index of the currently initialized element.
+	// Also, this functor can have an arbitrary number of other arguments - theArgs.
+	template <class Type, class Functor, class... ArgsType>
+	TVector<Type> CreateVector(const int theSize, Functor theFunc, ArgsType... theArgs)
+	{
+		TVector<Type> aVector(theSize);
+		for (int i = 0; i < theSize; ++i)
+		{
+			aVector[i] = theFunc(i, theArgs...);
+		}
+		return aVector;
+	}
+}
+
 TEST(TVector, can_create_vector_with_positive_length)
 {
   ASSERT_NO_THROW(TVector<int> v(5));
@@ -68,22 +97,18 @@ TEST(TVector, can_set_and_get_element)
 TEST(TVector, throws_when_set_element_with_negative_index)
 {
 	TVector<int> v(10);
-	ASSERT_ANY_THROW(v[-1]);
+	ASSERT_ANY_THROW(v[-1] = 0);
 }
 
 TEST(TVector, throws_when_set_element_with_too_large_index)
 {
 	TVector<int> v(10);
-	ASSERT_ANY_THROW(v[10]);
+	ASSERT_ANY_THROW(v[10] = 0);
 }
 
 TEST(TVector, can_assign_vector_to_itself)
 {
-	TVector<int> v(10);
-	for (int i = 0; i < 10; ++i)
-	{
-		v[i] = i;
-	}
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
 	TVector<int> v1(v);
 	v = v;
 	ASSERT_EQ(v, v1);
@@ -91,11 +116,7 @@ TEST(TVector, can_assign_vector_to_itself)
 
 TEST(TVector, can_assign_vectors_of_equal_size)
 {
-	TVector<int> v(10);
-	for (int i = 0; i < 10; ++i)
-	{
-		v[i] = i;
-	}
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
 	TVector<int> v1(10);
 	v1 = v;
 	ASSERT_EQ(v, v1);
@@ -103,11 +124,7 @@ TEST(TVector, can_assign_vectors_of_equal_size)
 
 TEST(TVector, assign_operator_change_vector_size)
 {
-	TVector<int> v(5);
-	for (int i = 0; i < 5; ++i)
-	{
-		v[i] = i;
-	}
+	TVector<int> v = CreateVector<int>(5, IdentityFunction<int>);
 	TVector<int> v1(10);
 	v1 = v;
 	ASSERT_EQ(5, v1.GetSize());
@@ -115,11 +132,7 @@ TEST(TVector, assign_operator_change_vector_size)
 
 TEST(TVector, can_assign_vectors_of_different_size)
 {
-	TVector<int> v(5);
-	for (int i = 0; i < 5; ++i)
-	{
-		v[i] = i;
-	}
+	TVector<int> v = CreateVector<int>(5, IdentityFunction<int>);
 	TVector<int> v1(10);
 	v1 = v;
 	ASSERT_EQ(v1, v);
@@ -127,56 +140,68 @@ TEST(TVector, can_assign_vectors_of_different_size)
 
 TEST(TVector, compare_equal_vectors_return_true)
 {
-	TVector<int> v(10);
-	for (int i = 0; i < 10; ++i)
-		v[i] = i;
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
 	TVector<int> v1(v);
 	ASSERT_TRUE(v == v1);
 }
 
 TEST(TVector, compare_vector_with_itself_return_true)
 {
-	TVector<int> v(10);
-	for (int i = 0; i < 10; ++i)
-		v[i] = i;
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
 	ASSERT_TRUE(v == v);
 }
 
 TEST(TVector, vectors_with_different_size_are_not_equal)
 {
-	TVector<int> v(10);
-	TVector<int> v1(12);
-	for (int i = 0; i < 10; ++i)
-	{
-		v[i] = i;
-		v1[i] = i;
-	}
+	TVector<int> v = CreateVector<int>(10, ConstantFunction<int>, 0);
+	TVector<int> v1 = CreateVector<int>(12, ConstantFunction<int>, 0);
 	ASSERT_NE(v, v1);
 }
 
 TEST(TVector, can_add_scalar_to_vector)
 {
-	TVector<int> v(10);
-	ASSERT_NO_THROW(v + 5);
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
+	int scalar = 10;
+
+	TVector<int> expected = CreateVector<int>(10, [](int i, int scalar) {return IdentityFunction<int>(i) + scalar; }, scalar);
+
+	TVector<int> actual = v + scalar;
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TVector, can_subtract_scalar_from_vector)
 {
-	TVector<int> v(10);
-	ASSERT_NO_THROW(v - 5);
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
+	int scalar = 10;
+
+	TVector<int> expected = CreateVector<int>(10, [](int i, int scalar) {return IdentityFunction<int>(i) - scalar; }, scalar);
+
+	TVector<int> actual = v - scalar;
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TVector, can_multiply_scalar_by_vector)
 {
-	TVector<int> v(10);
-	ASSERT_NO_THROW(v * 5);
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
+	int scalar = 10;
+
+	TVector<int> expected = CreateVector<int>(10, [](int i, int scalar) {return IdentityFunction<int>(i) * scalar; }, scalar);
+
+	TVector<int> actual = v * scalar;
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TVector, can_add_vectors_with_equal_size)
 {
-	TVector<int> v(10);
-	TVector<int> v1(10);
-	ASSERT_NO_THROW(v + v1);
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
+	int value = 5;
+	TVector<int> v1 = CreateVector<int>(10, ConstantFunction<int>, value);
+
+	TVector<int> expected = CreateVector<int>(10, [](int i, int constant) { return IdentityFunction<int>(i) + ConstantFunction(i, constant); }, value);
+
+	TVector<int> actual = v + v1;
+
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TVector, cant_add_vectors_with_not_equal_size)
@@ -188,9 +213,15 @@ TEST(TVector, cant_add_vectors_with_not_equal_size)
 
 TEST(TVector, can_subtract_vectors_with_equal_size)
 {
-	TVector<int> v(10);
-	TVector<int> v1(10);
-	ASSERT_NO_THROW(v - v1);
+	TVector<int> v = CreateVector<int>(10, IdentityFunction<int>);
+	int value = 5;
+	TVector<int> v1 = CreateVector<int>(10, ConstantFunction<int>, value);
+
+	TVector<int> expected = CreateVector<int>(10, [](int i, int constant) { return IdentityFunction<int>(i) - ConstantFunction(i, constant); }, value);
+
+	TVector<int> actual = v - v1;
+
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TVector, cant_subtract_vectors_with_not_equal_size)
@@ -202,9 +233,15 @@ TEST(TVector, cant_subtract_vectors_with_not_equal_size)
 
 TEST(TVector, can_multiply_vectors_with_equal_size)
 {
-	TVector<int> v(10);
-	TVector<int> v1(10);
-	ASSERT_NO_THROW(v * v1);
+	TVector<int> v = CreateVector<int>(5, IdentityFunction<int>);
+	int value = 6;
+	TVector<int> v1 = CreateVector<int>(5, ConstantFunction<int>, value);
+
+	int expected = (1 + 2 + 3 + 4) * 6;
+
+	int actual = v * v1;
+
+	ASSERT_EQ(actual, expected);
 }
 
 TEST(TVector, cant_multiply_vectors_with_not_equal_size)
